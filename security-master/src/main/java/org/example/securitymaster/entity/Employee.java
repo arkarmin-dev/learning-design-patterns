9370e3c09ba9d10447c80ae55b269e836c4edf8f
Arkar Min <akmin.dev@gmail.com>
2025-02-03 09:57:57 +0630

    Object Oriented Programming concepts in Java

---
+++ security-master/src/main/java/org/example/securitymaster/entity/Employee.java
@@ -0,0 +1,23 @@
+package org.example.securitymaster.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+    private String cubicleNo;
+}
---
+++ understanding-the-OOP-concepts-in-java/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.example.encapsulation.EncapsulationService.encapsulate;
+import static org.example.inheritance.InheritanceService.inherit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Testing Encapsulation in main method
+        encapsulate();
+        inherit();
+    }
+
+}
